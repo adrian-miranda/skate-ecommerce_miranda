0c3969633db83ad03783d20584d5e9141717450c
Adrian <adrian.miranda.r@gmail.com>
2021-10-21 19:15:00 -0300

    Se crea productos.json, modifica caracteristicas de Item , se crea estado y se usa UseEffect y UseState y setTimeOut para el llamado asincrono

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -1,19 +1,20 @@
 import {Card} from 'react-bootstrap'
 import { ItemCount } from '../ItemCount/ItemCount'
 import './Item.css'
-export const Item = ({stock}) =>{
+export const Item = ({stock , id , title , price , description , pictureUrl}) =>{
     return(
             <>
                 <Card className="col-md-10 col-lg-3 tarjeta">
-                    <Card.Img variant="top" src="holder.js/100px180" />
+                    <p>ID: {id}</p>
+                    <Card.Title>Titulo Producto : {title}</Card.Title>
+                    <Card.Text>Precio Producto : ${price}</Card.Text>
+                    <Card.Text>Descripcion : {description}</Card.Text>
+                    <Card.Img variant="top" src={pictureUrl} />
                     <Card.Body>
-                    <Card.Title>Nombre del articulo</Card.Title>
-                    <Card.Text>Descripcion del articulo
-                    </Card.Text>
-                        <ItemCount stock={stock}/>
-                        <button>
-                            <a href="{#}">Ver Detalle del producto</a>
-                        </button>
+                    <ItemCount stock={stock}/>
+                    <button>
+                        <a href="{#}">Ver Detalle del producto</a>
+                    </button>
                     </Card.Body>
                 </Card>
             </>
--- src/components/ItemList/ItemList.jsx
+++ src/components/ItemList/ItemList.jsx
@@ -1,12 +1,31 @@
 import './ItemList.css';
+import Productos from '../../Productos.json'
 import { Item } from '../Item/Item';
-export const ItemList = () =>{
+import { useState , useEffect } from 'react';
+
+export const ItemList = ({items}) =>{
+    const [productos , setProductos] = useState([]);
+    const cargaCatalogo = (data) => 
+    new Promise((resolve , reject) =>{
+        setTimeout(() =>{
+            if(data){
+                resolve(data)
+            }else{
+                reject('sin productos en stock')
+            }
+        },2000)
+    });
+    useEffect(()=>{
+        cargaCatalogo(Productos)
+        .then((respuesta)=> setProductos(respuesta))
+        .catch((error)=> console.log(error));
+    }, []);
     return(
         <>
-        <Item stock={6}/>
-        <Item />
-        <Item stock={1}/>
-        <Item stock={0}/>
-        </>
-    )
-}
+        {productos.length? productos.map((producto) => (
+            <Item key={producto.id} id={producto.id} title={producto.title} description={producto.description} price={producto.price}/>
+            )) : <h1>Cargando Catalogo</h1>}
+            </>
+            )
+        }
+console.log(Productos)
