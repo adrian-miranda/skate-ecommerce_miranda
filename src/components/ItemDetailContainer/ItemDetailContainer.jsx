c49c2cd42ef1c56ffc4edc6b4a9694f075bdd217
Adrian <adrian.miranda.r@gmail.com>
2021-11-02 20:53:02 -0300

    detalle

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -1,20 +1,14 @@
 import { useEffect, useState } from "react";
 import { ItemDetail } from "../ItemDetail/ItemDetail";
 
-
 export const ItemDetailContainer = ()=>{
-    const productoPrueba2 = [{id  : 9,name : "ProductoDePrueba1"},{id  : 10,name : "ProductoDePrueba2"}]
-    const filtrado = productoPrueba2.filter(filtrado2 => filtrado2.id === 10).map(filtrado2=> <h1>{filtrado2}</h1>)
-    console.log(`el objeto filtrado es: ${filtrado.name}`)
-    
     const productoPrueba = {
         id : 9,
         name : "ProductoDePrueba1",
         description : "Aca va la descripcion del producto",
         imagen : "https://m.media-amazon.com/images/I/61VX2hb1l5L._AC_SY741_.jpg"
     }
-    
-    const [detailProducto , setDetailProducto] = useState(productoPrueba)
+    const [detailProducto , setDetailProducto] = useState('')
     const getItem = (data) =>
     new Promise((resolve , reject)=>{
         setTimeout(() =>{
@@ -30,12 +24,9 @@ export const ItemDetailContainer = ()=>{
         getItem(detailProducto)
         .then((respuesta)=> setDetailProducto(respuesta))
         .catch((error)=> console.log(error))
-        console.log(`el producto es: ${productoPrueba}`)
+        console.log(`el producto es: ${productoPrueba.name}`)
     } , [])
     return(
-        <>
-            <ItemDetail productoPrueba={productoPrueba} />
-        </>
+        <><ItemDetail productoPrueba={productoPrueba} /></>
     )
-    
 }
